f6f6971aa0823263db8ae6fda4e147d58b30d340
Student <example@example.com>
2019-09-16 15:32:19 -0700

    Rewrite with ES6

--- src/yelp-api.js
+++ src/yelp-api.js
@@ -13,32 +13,28 @@ export class BusinessSearch {
       dataType: 'json',
       success: function(data){
         // Grab the results from the API JSON return
-        const totalresults = data.business
-
+        const totalResults = data.businesses;
+        console.log({totalResults});
         // If our results are greater than 0, continue
-        if (totalresults > 0){
-          // Display a header on the page with the number of results
-          $('#results').append('<h5>We discovered ' + totalresults + ' results!</h5>');
+        if (totalResults.length > 0){
           // Itirate through the JSON array of 'businesses' which was returned by the API
           $.each(data.businesses, function(i, item) {
             // Store each business's object in a variable
-            var id = item.id;
-            var alias = item.alias;
-            var phone = item.display_phone;
-            var image = item.image_url;
-            var name = item.name;
-            var rating = item.rating;
-            var reviewcount = item.review_count;
-            var address = item.location.address1;
-            var city = item.location.city;
-            var state = item.location.state;
-            var zipcode = item.location.zip_code;
+            const id = item.id;
+            const alias = item.categories[0].title;
+            const phone = item.display_phone;
+            const name = item.name;
+            const rating = item.rating;
+            const reviewCount = item.review_count;
+            const address = item.location.address1;
+            const city = item.location.city;
+            const state = item.location.state;
             // Append our result into our page
-            $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
+            $('#business-info').append(`<div id="${id}"><b>${name}</b><br>Rate: ${rating}/5 ${reviewCount} Reviews<br>Category: ${alias}<br>${address}, ${city}, ${state}<br>${phone}</div>`);
           });
         } else {
           // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
-          $('#results').append('<h5>We discovered no results!</h5>');
+          $('#business-info').append('<h5>We discovered no results!</h5>');
         }
       }
     });

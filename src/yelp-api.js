3dc6f5a336054beabfbf11b535407ec2f432159a
Student <example@example.com>
2019-09-17 09:35:10 -0700

    Update variable names from url to link and name to businessName

--- src/yelp-api.js
+++ src/yelp-api.js
@@ -24,23 +24,23 @@ export class BusinessSearch {
           $.each(totalResults, function(i, item) {
             // Store each business's object in a variable
             const id = item.id;
-            const url = item.url;
+            const link = item.url;
             const alias = item.categories[0].title;
             const phone = item.display_phone;
-            const name = item.name;
+            const businessName = item.name;
             const rating = item.rating;
             const reviewCount = item.review_count;
             const address = item.location.address1;
             const city = item.location.city;
             const state = item.location.state;
-            // Append our result into our page
-            $('#business-info').append(`
-              <div id="${id}">
-                <a href="${url}"><p>${name}</p></a>
+            $('#business-info').append(
+              `<div id="${id}">
+                <a href="${link}"><p>${businessName}</p></a>
                 Rate: ${rating}/5  with ${reviewCount} Reviews<br>Category: ${alias}<br>
                 ${address}, ${city}, ${state}<br>
                 ${phone}<hr>
-              </div>`);
+              </div>`
+            );
           });
         } else {
           // If our results are 0; display a no result message.

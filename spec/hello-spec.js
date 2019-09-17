1f6c2aee9a449fd008f84c912553934e37c91f5a
Vera Weikel <vera.weikel@gmail.com>
2019-09-17 01:21:43 -0700

    Update unit tests.

--- spec/hello-spec.js
+++ spec/hello-spec.js
@@ -11,61 +11,64 @@
     - given the city, does it return city specific pics
 */
 
-import { hello } from '../src/main';
 import { WeatherService } from '../src/weather-service';
-//import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import $ from 'jquery';
+import { promises } from 'fs';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
 
-describe('hello', function() {
-  it('should return hello', function() {
-    var message = hello();
-    expect(message).toEqual("Hello");
-  });
-  let weatherService = new WeatherService();
+let weatherService = new WeatherService();
   //TDD 
-  //return valid city (entered by user) pertinent info
-  it("return valid city (entered by user) pertinent info", function(){
-    //method test
-      expect(weatherService.getWeatherByCity(city)[0]).toEqual("The complement to the object.response.");  
-  });
-  //valid GET request at given endpoint (each API)
+  //return valid city (entered by user) pertinent info //no way to viably access the input
+describe('city', function() {
+//   it('should return city', function() {
+//     var city = $('#location').val();
+//     console.log(city, weatherService.city);
+//     expect(weatherService.city).toEqual("city");
+//   });
+
+//   //valid GET request at given endpoint (each API)
   it("valid GET request at given endpoint (each API)", function(){
-    //method test
-    expect(message).toEqual("Hello");
+    //method test: declare promise and call
+    let promise = weatherService.getWeatherByCity("chicago");
+    return promise.then(function(response){ //in a test environ w/async, must do a return callback;
+      const body = JSON.parse(response);
+      expect(body.name).toEqual("chicago"); 
+    })
+
   });
-  //isValid json obj information retreived at status 200
-  it("isValid json obj information retreived at status 200", function(){
-    //method test
-    expect(message).toEqual("Hello");
-  });
-  // given the city, does it return the news
-  it("given the city, does it return the news", function(){
-    //method test
-    expect(message).toEqual("Hello");
-  });
-  // - given the city, does it return the restaurants
-  it("given the city, does it return the restaurants", function(){
-    //method test
-    expect(message).toEqual("Hello");
-  });
-  // - given the city, does it return the weather 
-  it("given the city, does it return the weather", function(){
-    let cityWeather = weatherService.getWeatherByCity()
-    //method test
-    expect(weatherService.getWeatherByCity(city)).toEqual("38 C");
-  });
-  // - given the city, does it return the map
-  it("given the city, does it return the map", function(){
-    //method test
-    expect(message).toEqual("Hello");
-  });
-  // - given the city, does it return city specific pics
-  it("given the city, does it return city specific pics", function(){
-    //method test
-    expect(message).toEqual("Hello");
-  });
+//   //isValid json obj information retreived at status 200
+//   it("isValid json obj information retreived at status 200", function(){
+//     //method test
+//     expect(message).toEqual("Hello");
+//   });
+//   // given the city, does it return the news
+//   it("given the city, does it return the news", function(){
+//     //method test
+//     expect(message).toEqual("Hello");
+//   });
+//   // - given the city, does it return the restaurants
+//   it("given the city, does it return the restaurants", function(){
+//     //method test
+//     expect(message).toEqual("Hello");
+//   });
+//   // - given the city, does it return the weather 
+//   it("given the city, does it return the weather", function(){
+//     let cityWeather = weatherService.getWeatherByCity()
+//     //method test
+//     expect(weatherService.getWeatherByCity(city)).toEqual("38 C");
+//   });
+//   // - given the city, does it return the map
+//   it("given the city, does it return the map", function(){
+//     //method test
+//     expect(message).toEqual("Hello");
+//   });
+//   // - given the city, does it return city specific pics
+//   it("given the city, does it return city specific pics", function(){
+//     //method test
+//     expect(message).toEqual("Hello");
+//   });
 });
 
 

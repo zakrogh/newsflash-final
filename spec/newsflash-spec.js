5a8a1b08aa179849112ad874d0df2db96e0b77ab
Vera Weikel <vera.weikel@gmail.com>
2019-09-18 21:46:05 -0700

    NewsFlash: City I) valid GET request at given endpoint (each API) II) Is typeof str? valid gatherWeather(?) from JSON.res III) Given city, is weather req.res valid from given endpoint temp: isNum? IV) given the city, does it return the news V) valid req.res from given news Image from endpoint: isStr VI) given the city, does it return the restaurants Business API should load and render business Api call when document is ready

--- spec/newsflash-spec.js
+++ spec/newsflash-spec.js
@@ -16,7 +16,7 @@ import { getNewsApi } from '../src/news-api';
 import { WeatherService } from '../src/weather-service';
 
 let weatherService = new WeatherService();
-let businessSearch = new BusinessSearch("chicago, restaurant"); 
+let businessSearch = new BusinessSearch("chicago", "restaurant"); 
 
 //  TDD 
 describe('NewsFlash: City', function() {
@@ -26,7 +26,7 @@ describe('NewsFlash: City', function() {
 //valid GET request at given endpoint (each API)
   it("I) valid GET request at given endpoint (each API)", function(){
     //method test: declare promise and call
-    let promise = weatherService.getWeatherByCity("chicago");
+    let promise = weatherService.getWeatherByCoords(41.8781, -87.6298);
     return promise.then(function(response){ //in a test environment w/async, must do a return callback;
       const body = JSON.parse(response);
       expect(body.name.toLowerCase()).toEqual("chicago"); 
@@ -50,7 +50,7 @@ describe('NewsFlash: City', function() {
   //   })
   // });
    it("II) Is typeof str? valid gatherWeather(?) from JSON.res", function(){
-    let promise = weatherService.getWeatherByCity("Chicago");
+    let promise = weatherService.getWeatherByCoords(41.8781, -87.6298);
     return promise.then(function(response){
       const body = JSON.parse(response);
       expect(typeof body.weather[0].description).toEqual("string");
@@ -66,7 +66,7 @@ describe('NewsFlash: City', function() {
 //Due to weather changing so rapidly/drastically:  - Given city gotten does it return weather in correct JSON req.res format
   it("III) Given city, is weather req.res valid from given endpoint temp: isNum? ", function(){
       //method test: 
-      let promise = weatherService.getWeatherByCity("chicago");
+      let promise = weatherService.getWeatherByCoords(41.8781, -87.6298);
       return promise.then(function(response){ //in a test environ w/async, must do a return callback;
       const body = JSON.parse(response);
       expect(typeof body.main.temp).toEqual("number"); 
@@ -94,10 +94,13 @@ describe('NewsFlash: City', function() {
 // Biz test; given the city, does it return the restaurants
   it("VI) given the city, does it return the restaurants", function(){
     //method test: 
-    let promise = businessSearch.callBusinessInfo("restaurant");
-    return promise.then(function(item){ //in a test environ w/async, must do a return callback;
-  //    const item = JSON.parse(response);
-      expect(typeof item.categories[0]).toEqual("string"); 
+    console.log(businessSearch);
+    let promise = businessSearch.callBusinessInfo();
+    console.log(promise);
+      return promise.then(function(item){ //in a test environ w/async, must do a return callback;
+      console.log(item)
+      //  const item = JSON.parse(response);
+      expect(typeof item[0].categories[0].title).toEqual("string"); 
     });
   });
   // MAP TEST // - 
